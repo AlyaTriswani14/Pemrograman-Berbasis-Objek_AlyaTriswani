3df39ecb6e593fcf5f1d20a100ee2a86236969fb
Alya Triswani <153605534+AlyaTriswani14@users.noreply.github.com>
2025-11-25 09:35:58 +0700

    Add files via upload

---
+++ 2425-ge-m01-advanced-transactions-sintonghutapea/src/fintech/model/Account.java
@@ -0,0 +1,38 @@
+package fintech.model;
+
+/**
+ * @author 12S23029 Sintong Hutapea
+ * @author 12S23038 Alya Triswani
+ */
+
+
+ public class Account {
+ 
+     private String owner;
+     private String accountName;
+     private double balance;
+ 
+     public Account(String owner, String accountName) {
+         this.owner = owner;
+         this.accountName = accountName;
+         this.balance = 0.0;
+     }
+ 
+     public void addBalance(double amount) {
+         this.balance += amount;
+     }
+ 
+     public String getAccountName() {
+         return accountName;
+     }
+ 
+     public double getBalance() {
+         return balance;
+     }
+ 
+     @Override
+     public String toString() {
+         return owner + "|" + accountName + "|" + balance;
+     }
+ }
+ 
